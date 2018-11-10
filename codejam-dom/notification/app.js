a57f1da3623a85e7d28a73fa4370176e9ab2ef0b
Vladislav Drobovich <vladdrobovich@gmail.com>
2018-11-10 22:55:18 +0300

    feat: add tips content

--- codejam-dom/notification/app.js
+++ codejam-dom/notification/app.js
@@ -2,4 +2,10 @@ setTimeout(() => {
 	tipsFunc();
 }, 5000);
 
-const strMas = [];
+const strMas = ['Be Flexible. We always plan for delays and try not to get upset when things inevitably go wrong.',
+	'Always Buy Travel Insurance. A medical emergency can wipe out your savings — or even worse.',
+	'Keep an Open Mind. Don’t judge other customs. You are a visitor. Be respectful.',
+	'Leave Room for Spontaneity. Unplanned moments while traveling can be the best memories.',
+	'Let Someone at Home Know Your Plans. This is extremely important when traveling solo.',
+	'Wear Sunscreen. It’s especially important in hot countries.',
+];
