7df2512818dd6bf00c3ab0165f12940454ce5d9d
Vladislav Drobovich <vladdrobovich@gmail.com>
2018-11-11 12:18:47 +0300

    feat: add disable notification

--- codejam-dom/notification/app.js
+++ codejam-dom/notification/app.js
@@ -1,7 +1,3 @@
-setTimeout(() => {
-	tipsFunc();
-}, 5000);
-
 const strMas = ['Be Flexible. We always plan for delays and try not to get upset when things inevitably go wrong.',
 	'Always Buy Travel Insurance. A medical emergency can wipe out your savings — or even worse.',
 	'Keep an Open Mind. Don’t judge other customs. You are a visitor. Be respectful.',
@@ -9,3 +5,24 @@ const strMas = ['Be Flexible. We always plan for delays and try not to get upset
 	'Let Someone at Home Know Your Plans. This is extremely important when traveling solo.',
 	'Wear Sunscreen. It’s especially important in hot countries.',
 ];
+
+function tipsFunc() {
+	const notification = document.getElementsByClassName('notification')[0];
+	const isChecked = document.getElementById('checkboxTips');
+	function closeNotif() {
+		notification.classList.add('hidden');
+		if (isChecked.checked) {
+			localStorage.setItem('openNotification', 'false');
+		}
+	}
+	const open = localStorage.getItem('openNotification');
+	if (open !== 'false') {
+		notification.classList.remove('hidden');
+		const exit = document.getElementsByClassName('closeNotification')[0];
+		exit.addEventListener('click', closeNotif);
+	}
+}
+
+setTimeout(() => {
+	tipsFunc();
+}, 1000);
