0c1c86e08f7cecc330610d2f5718a57b38764a1f
Vladislav Drobovich <vladdrobovich@gmail.com>
2018-11-11 14:04:13 +0300

    feat: add Ðdynamic points

--- codejam-dom/notification/app.js
+++ codejam-dom/notification/app.js
@@ -9,17 +9,38 @@ const strMas = ['Be Flexible. We always plan for delays and try not to get upset
 function tipsFunc() {
 	const notification = document.getElementsByClassName('notification')[0];
 	const isChecked = document.getElementById('checkboxTips');
+	//
 	function closeNotif() {
 		notification.classList.add('hidden');
 		if (isChecked.checked) {
 			localStorage.setItem('openNotification', 'false');
 		}
 	}
+	//
+	let count = 0;
+	function createElement() {
+		count += 1;
+		const div = document.createElement('div');
+		div.id = count;
+		div.className = 'tip';
+		return div;
+	}
+	//
 	const open = localStorage.getItem('openNotification');
 	if (open !== 'false') {
 		notification.classList.remove('hidden');
 		const exit = document.getElementsByClassName('closeNotification')[0];
 		exit.addEventListener('click', closeNotif);
+		//
+		const i = 0;
+		const notTxt = document.getElementById('textNotification');
+		notTxt.innerHTML = strMas[i];
+		//
+		const tips = document.getElementById('tips');
+		//
+		strMas.map(() => tips.appendChild(createElement()));
+		tips.childNodes[0].classList.add('chosenTip');
+		//
 	}
 }
 
